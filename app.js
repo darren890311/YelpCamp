37198eca220aa8ef79b975ef2823e0c6032de39e
darren <darren890311@gmail.com>
2024-07-12 03:55:13 +0800

    Update app.js to use environment variables2

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 const MongoStore = require("connect-mongo");
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
